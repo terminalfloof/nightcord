a0266ec0a69a1030cfe3f3c9f9c2b33ec988e091
floof <rglyxr@gmail.com>
2024-04-26 00:07:04 -0500

    hopefully final change, listen to correct PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -39,6 +39,8 @@ io.on("connection", async (socket) => {
 
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-httpServer.listen(3000, () => {
-	console.log("Server is running on port 3000.");
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
 });
